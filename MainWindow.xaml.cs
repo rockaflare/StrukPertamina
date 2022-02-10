132b6e54560b1df32615cd4545c67ae8787020f7
raziqin.ninetynine <raziqin.ninetynine@gmail.com>
2022-02-10 20:16:06 +0800

    Initial

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
+using System.Collections.Specialized;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Drawing.Printing;
+using System;
+using System.Printing;
 
 namespace StrukPertamina
 {
@@ -23,6 +15,63 @@ namespace StrukPertamina
         public MainWindow()
         {
             InitializeComponent();
+            GetPrinterList();
+            DisplayStrukConfig();
+        }
+
+        
+
+        private void GetPrinterList()
+        {
+            PrinterSelect.Items.Add("Pilih Printer..");
+            String pkInstalledPrinters;
+            for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
+            {
+                pkInstalledPrinters = PrinterSettings.InstalledPrinters[i];
+                PrinterSelect.Items.Add(pkInstalledPrinters);
+            }
+            PrinterSelect.SelectedIndex = 0;
+        }
+
+        private void DisplayStrukConfig()
+        {
+            Nama.Text = ConfigurationManager.AppSettings.Get("Nama");
+            Alamat.Text = ConfigurationManager.AppSettings.Get("Alamat");
+            RecNo.Text = ConfigurationManager.AppSettings.Get("RecNo");
+            HoseNo.Text = ConfigurationManager.AppSettings.Get("Hose");
+            Products.Text = ConfigurationManager.AppSettings.Get("DEXLITE");
+            Harga.Text = ConfigurationManager.AppSettings.Get("Harga");
+            Prov.Text = ConfigurationManager.AppSettings.Get("Prov");
+            Footer.Text = ConfigurationManager.AppSettings.Get("Footer");
+        }
+
+        private void SaveStrukConfig(string key, string value)
+        {
+            try
+            {
+                Configuration configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if(settings[key] == null)
+                {
+                    settings.Add(key,value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+
+                MessageBox.Show("Error menyimpan data");
+            }
+        }
+
+        private void Nama_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            SaveStrukConfig("Nama", Nama.Text);
         }
     }
 }
---
+++ StrukModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrukPertamina
+{
+    public class StrukModel
+    {
+        public string Nama { get; set; }
+        public string Alamat { get; set; }
+        public string ReceiptNo { get; set; }
+        public string Hose { get; set; }
+        public string Produk { get; set; }
+        public int Harga { get; set; }
+
+        public string Prov { get; set; }
+        public string Footer { get; set; }
+    }
+}
